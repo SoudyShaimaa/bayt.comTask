7bdb6646d3966f73697294d228cf6ccec805fedc
SoudyShaimaa <shaimaa.soudy@gmail.com>
2018-03-12 02:44:49 +0200

    final update

---
+++ Html/js/mainCustom.js
@@ -0,0 +1,19 @@
+/**
+ * jTinder initialization
+ */
+$("#tinderslide").jTinder({
+
+	animationRevertSpeed: 200,
+	animationSpeed: 400,
+	threshold: 1,
+	likeSelector: '.like',
+	dislikeSelector: '.dislike'
+});
+
+/**
+ * Set button action to trigger jTinder like & dislike.
+ */
+$('.actions .like, .actions .dislike').click(function(e){
+	e.preventDefault();
+	$("#tinderslide").jTinder($(this).attr('class'));
+});
